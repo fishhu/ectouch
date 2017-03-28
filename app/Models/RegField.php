b22ee1f545c17e37c043eb67be066650492bbfda
carson <docxcn@gmail.com>
2017-03-28 15:30:18 +0800

    增加模型文件

---
+++ app/Models/RegField.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class RegField
+ */
+class RegField extends Model
+{
+    protected $table = 'reg_fields';
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'reg_field_name',
+        'dis_order',
+        'display',
+        'type',
+        'is_need'
+    ];
+
+    protected $guarded = [];
+
+        
+}
