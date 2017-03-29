c1f30162769f663fbdcc41827928d4ec09415b4c
carson <docxcn@gmail.com>
2017-03-29 16:11:27 +0800

    更新mix配置

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,5 +13,4 @@ const { mix } = require('laravel-mix');
 
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css')
-   .js('resources/assets/js/admin/dashboard.js', 'public/js')
-   .sass('resources/assets/sass/admin/dashboard.scss', 'public/css');
+   .js('resources/assets/js/admin/dashboard.js', 'public/js');
