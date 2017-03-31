53d2fec904a1dc419289ae8cdd3de60205acdcb6
carson <docxcn@gmail.com>
2017-03-31 15:08:02 +0800

    优化模板标签

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,9 +2,13 @@
 <html lang="{{ config('app.locale') }}">
     <head>
         <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name=viewport content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=0">
         <title>微商城</title>
+        <meta name="description" content=""/>
+        <meta name="keywords" content=""/>
+        <meta name="format-detection" content="telephone=no"/>
+        <meta name="apple-mobile-web-app-capable" content="yes"/>
+        <meta name="apple-mobile-web-app-status-bar-style" content="black"/>
         <script>
             window.Laravel = <?php echo json_encode([
                 'csrfToken' => csrf_token(),
