71afbb6263695ab004649a3d301a9e5f2f7d472a
carson <docxcn@gmail.com>
2017-04-06 12:02:51 +0800

    upgrade

---
+++ app/Http/Controllers/Mall/Api/ArticleController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Mall\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ArticleController extends Controller
+{
+    //
+}
---
+++ app/Http/Controllers/Shop/Api/VirtualcodeController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Shop\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class VirtualcodeController extends Controller
+{
+    //
+}
