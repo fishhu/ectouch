1b18656491f6a2434fba2dec7ceaef96e23ac127
carson <docxcn@gmail.com>
2017-04-07 17:25:15 +0800

    更新 ShopConfig 模型

--- app/Models/ShopConfig.php
+++ app/Models/ShopConfig.php
@@ -25,5 +25,42 @@ class ShopConfig extends Model
 
     protected $guarded = [];
 
-        
+    /**
+     * @param $code
+     * @return mixed
+     */
+    public static function findByCode($code)
+    {
+        return self::where(['code' => $code])->value('value');
+    }
+
+    /**
+     * @return array
+     */
+    public static function getShopInfo()
+    {
+        return [
+            'site_info' => [
+                'name' => self::findByCode('shop_name'),
+                'desc' => self::findByCode('shop_desc'),
+                'logo' => formatPhoto(self::findByCode('shop_logo')),
+                'opening' => (bool)!self::findByCode('shop_closed'),
+                'telephone' => self::findByCode('service_phone'),
+            ]
+        ];
+    }
+
+    /**
+     * @param $configure
+     * @return array
+     */
+    public static function getConfigure($configure)
+    {
+        $data = [];
+        $configure = unserialize($configure);
+        foreach ($configure as $key => $val) {
+            $data[$val['name']] = $val['value'];
+        }
+        return $data;
+    }
 }
