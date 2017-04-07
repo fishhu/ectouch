a4293e100c65e6e53b17117d4559678bff6bc301
carson <docxcn@gmail.com>
2017-04-07 16:57:28 +0800

    更新路由模式

--- routes/api.php
+++ routes/api.php
@@ -32,13 +32,13 @@ Route::group(['prefix' => 'v2', 'middleware' => 'api'], function () {
     Route::post('ectouch.item.accessory.list', 'GoodsController@accessory'); // 获取商品配件
     Route::post('ectouch.items.get', 'GoodsController@index'); // 获取商品列表
     // 类目
-    Route::get('ectouch.category.get', 'CategoryController@show'); // 获取单个分类信息
-    Route::get('ectouch.categories.get', 'CategoryController@index'); // 获取分类列表
-    Route::get('ectouch.brand.category.get', 'BrandController@category'); // 获取品牌分类
-    Route::get('ectouch.brand.get', 'BrandController@show'); // 获取单个品牌信息
-    Route::get('ectouch.brands.get', 'BrandController@index'); // 获取品牌列表
+    Route::post('ectouch.category.get', 'CategoryController@show'); // 获取单个分类信息
+    Route::post('ectouch.categories.get', 'CategoryController@index'); // 获取分类列表
+    Route::post('ectouch.brand.category.get', 'BrandController@category'); // 获取品牌分类
+    Route::post('ectouch.brand.get', 'BrandController@show'); // 获取单个品牌信息
+    Route::post('ectouch.brands.get', 'BrandController@index'); // 获取品牌列表
     // 用户
-    Route::get('ectouch.user.signup.fields', 'UserController@fields'); // 获取注册字段
+    Route::post('ectouch.user.signup.fields', 'UserController@fields'); // 获取注册字段
     Route::post('ectouch.user.social', 'UserController@social'); // 获取oAuth
     Route::post('ectouch.user.oauth', 'UserController@oauth'); // oAuth登录
     Route::post('ectouch.user.signin', 'UserController@signin'); // 用户登录
@@ -50,47 +50,47 @@ Route::group(['prefix' => 'v2', 'middleware' => 'api'], function () {
     Route::post('ectouch.user.mobile.reset', 'UserController@resetByMobile');
     Route::post('ectouch.user.bind', 'UserController@bind'); // 绑定注册
     // 评论
-    Route::get('ectouch.comments.get', 'CommentController@index'); // 显示评论列表
-    Route::get('ectouch.messages.get', 'MessageController@index'); // 显示留言列表
+    Route::post('ectouch.comments.get', 'CommentController@index'); // 显示评论列表
+    Route::post('ectouch.messages.get', 'MessageController@index'); // 显示留言列表
     // 营销
-    Route::get('ectouch.activity.list', 'ActivityController@index'); // 优惠活动
-    Route::get('ectouch.auction.list', 'AuctionController@index'); // 拍卖活动
-    Route::get('ectouch.groupbuy.list', 'GroupBuyController@index'); // 团购活动
-    Route::get('ectouch.exchange.list', 'ExchangeController@index'); // 积分兑换
-    Route::get('ectouch.topic.list', 'TopicController@index'); // 专题汇
-    Route::get('ectouch.article.list', 'ArticleController@index'); // 社区资讯
-    Route::get('ectouch.article.get', 'ArticleController@show');
-    Route::get('ectouch.wholesale.list', 'WholesaleController@index'); // 批发活动
-    Route::get('ectouch.snatch.list', 'SnatchController@index'); // 夺宝奇兵
+    Route::post('ectouch.activity.list', 'ActivityController@index'); // 优惠活动
+    Route::post('ectouch.auction.list', 'AuctionController@index'); // 拍卖活动
+    Route::post('ectouch.groupbuy.list', 'GroupBuyController@index'); // 团购活动
+    Route::post('ectouch.exchange.list', 'ExchangeController@index'); // 积分兑换
+    Route::post('ectouch.topic.list', 'TopicController@index'); // 专题汇
+    Route::post('ectouch.article.list', 'ArticleController@index'); // 社区资讯
+    Route::post('ectouch.article.get', 'ArticleController@show');
+    Route::post('ectouch.wholesale.list', 'WholesaleController@index'); // 批发活动
+    Route::post('ectouch.snatch.list', 'SnatchController@index'); // 夺宝奇兵
     // 微信
     Route::post('ectouch.wechat.jssdk', 'WechatController@jssdk'); // 微信JSSDK
     // 门店
-    Route::get('ectouch.stores.get', 'StoreController@index'); // 门店列表
-    Route::get('ectouch.store.get', 'StoreController@show'); // 门店详情
+    Route::post('ectouch.stores.get', 'StoreController@index'); // 门店列表
+    Route::post('ectouch.store.get', 'StoreController@show'); // 门店详情
     // 核销
-    Route::get('ectouch.virtualcodes.get', 'VirtualcodeController@index'); // 核销订单列表
-    Route::get('ectouch.virtualcode.get', 'VirtualcodeController@show'); // 核销订单信息
+    Route::post('ectouch.virtualcodes.get', 'VirtualcodeController@index'); // 核销订单列表
+    Route::post('ectouch.virtualcode.get', 'VirtualcodeController@show'); // 核销订单信息
     Route::post('ectouch.virtualcode.apply', 'VirtualcodeController@apply'); // 使用核销验证码
     // 店铺
-    Route::get('ectouch.shop.config', 'ShopController@config'); // 系统配置
-    Route::get('ectouch.shop.shipping', 'ShopController@shipping'); // 配送方式
-    Route::get('ectouch.shop.payment', 'ShopController@payment'); // 支付方式
-    Route::get('ectouch.shop.banner', 'ShopController@banner'); // 手机幻灯片
-    Route::get('ectouch.shop.adv', 'ShopController@adv'); // 手机广告
-    Route::get('ectouch.shop.help', 'ShopController@help'); // 商店帮助
+    Route::post('ectouch.shop.config', 'ShopController@config'); // 系统配置
+    Route::post('ectouch.shop.shipping', 'ShopController@shipping'); // 配送方式
+    Route::post('ectouch.shop.payment', 'ShopController@payment'); // 支付方式
+    Route::post('ectouch.shop.banner', 'ShopController@banner'); // 手机幻灯片
+    Route::post('ectouch.shop.adv', 'ShopController@adv'); // 手机广告
+    Route::post('ectouch.shop.help', 'ShopController@help'); // 商店帮助
     // 工具
-    Route::get('ectouch.tool.region', 'MiscController@region'); // 地区
-    Route::get('ectouch.search.keywords', 'GoodsController@index'); // 搜索关键词
+    Route::post('ectouch.tool.region', 'MiscController@region'); // 地区
+    Route::post('ectouch.search.keywords', 'GoodsController@index'); // 搜索关键词
 });
 
 // Authentication
 Route::group(['prefix' => 'v2', 'middleware' => 'auth:api'], function () {
     // 用户资料
-    Route::get('ectouch.user.get', 'UserController@show'); // 获取用户信息
+    Route::post('ectouch.user.get', 'UserController@show'); // 获取用户信息
     Route::post('ectouch.user.profile.update', 'UserController@update'); // 更新用户资料
     Route::post('ectouch.user.password.update', 'UserController@password'); // 修改会员密码
     // 收货地址
-    Route::get('ectouch.user.address.get', 'UserAddressController@show'); // 获取用户收货地址
+    Route::post('ectouch.user.address.get', 'UserAddressController@show'); // 获取用户收货地址
     Route::post('ectouch.user.address.add', 'UserAddressController@store'); // 添加用户收货地址
     Route::post('ectouch.user.address.update', 'UserAddressController@update'); // 更新用户收货地址
     Route::post('ectouch.user.address.delete', 'UserAddressController@destroy'); // 删除用户收货地址
@@ -98,30 +98,30 @@ Route::group(['prefix' => 'v2', 'middleware' => 'auth:api'], function () {
     // 收藏&关注
     Route::post('ectouch.user.collect.add', 'UserCollectController@store'); // 用户收藏单个商品
     Route::post('ectouch.user.collect.delete', 'UserCollectController@destroy'); // 用户删除收藏商品
-    Route::get('ectouch.user.collects.get', 'UserCollectController@index'); // 用户收藏列表
+    Route::post('ectouch.user.collects.get', 'UserCollectController@index'); // 用户收藏列表
     Route::post('ectouch.user.attention.add', 'UserAttentionController@store'); // 添加关注商品
     Route::post('ectouch.user.attention.delete', 'UserAttentionController@destroy'); // 取消关注商品
-    Route::get('ectouch.user.attentions.get', 'UserAttentionController@index'); // 用户关注列表
+    Route::post('ectouch.user.attentions.get', 'UserAttentionController@index'); // 用户关注列表
     // 资金
-    Route::get('ectouch.user.account.log', 'UserAccountController@log'); // 获取会员充值提现记录
+    Route::post('ectouch.user.account.log', 'UserAccountController@log'); // 获取会员充值提现记录
     Route::post('ectouch.user.account.deposit', 'UserAccountController@deposit'); // 创建会员充值申请
     Route::post('ectouch.user.account.raply', 'UserAccountController@raply'); // 创建会员提现申请
-    Route::get('ectouch.user.account.detail', 'UserAccountController@detail'); // 获取帐户资金明细
+    Route::post('ectouch.user.account.detail', 'UserAccountController@detail'); // 获取帐户资金明细
     Route::post('ectouch.user.account.pay', 'UserAccountController@pay'); // 会员充值付款
     Route::post('ectouch.user.account.cancel', 'UserAccountController@cancel'); // 会员充值/提现申请取消
     // 红包
-    Route::get('ectouch.user.bonus.get', 'UserBonusController@index'); // 会员红包列表
+    Route::post('ectouch.user.bonus.get', 'UserBonusController@index'); // 会员红包列表
     Route::post('ectouch.user.bonus.add', 'UserBonusController@store'); // 添加一个红包
     // 缺货登记
     Route::post('ectouch.user.booking.add', 'UserBookingController@store'); // 添加缺货登记
     Route::post('ectouch.user.booking.delete', 'UserBookingController@destroy'); // 删除缺货登记
-    Route::get('ectouch.user.bookings.get', 'UserBookingController@index'); // 显示缺货登记列表
+    Route::post('ectouch.user.bookings.get', 'UserBookingController@index'); // 显示缺货登记列表
     // 标签云
-    Route::get('ectouch.user.tags.get', 'UserTagController@index'); // 标签云列表
+    Route::post('ectouch.user.tags.get', 'UserTagController@index'); // 标签云列表
     Route::post('ectouch.user.tag.add', 'UserTagController@store'); // 添加标签云
     Route::post('ectouch.user.tag.delete', 'UserTagController@destroy'); // 删除标签
     // 推荐分成
-    Route::get('ectouch.user.affiliate', 'UserController@affiliate'); // 用户推荐分成
+    Route::post('ectouch.user.affiliate', 'UserController@affiliate'); // 用户推荐分成
     // 浏览历史
     Route::post('ectouch.user.history.add', 'UserHistoryController@store'); // 增加商品浏览历史
     Route::post('ectouch.user.history.clear', 'UserHistoryController@clear'); // 清除商品浏览历史
@@ -131,7 +131,7 @@ Route::group(['prefix' => 'v2', 'middleware' => 'auth:api'], function () {
     Route::post('ectouch.message.add', 'MessageController@store'); // 提交留言反馈
     Route::post('ectouch.message.delete', 'MessageController@destroy'); // 删除留言
     // 购物车
-    Route::get('ectouch.cart.get', 'CartController@index'); // 获取购物车商品
+    Route::post('ectouch.cart.get', 'CartController@index'); // 获取购物车商品
     Route::post('ectouch.cart.add', 'CartController@store'); // 添加一个商品到购物车
     Route::post('ectouch.cart.update', 'CartController@update'); // 更新购物车商品数目
     Route::post('ectouch.cart.delete', 'CartController@destroy'); // 从购物车中删除一商品
@@ -153,14 +153,14 @@ Route::group(['prefix' => 'v2', 'middleware' => 'auth:api'], function () {
     Route::post('ectouch.flow.oos.update', 'FlowController@oos'); // 改变缺货处理方式
     // 订单
     Route::post('ectouch.order.add', 'OrderController@store'); // 提交订单数据
-    Route::get('ectouch.order.get', 'OrderController@show'); // 获取单个订单
+    Route::post('ectouch.order.get', 'OrderController@show'); // 获取单个订单
     Route::post('ectouch.order.update', 'OrderController@update'); // 更新单个订单
     Route::post('ectouch.order.cancel', 'OrderController@cancel'); // 取消单个订单
     Route::post('ectouch.order.merge', 'OrderController@merge'); // 合并两个订单
     Route::post('ectouch.order.again', 'OrderController@again'); // 订单商品添加到购物车
-    Route::get('ectouch.order.pay', 'OrderController@pay'); // 获取订单支付信息
+    Route::post('ectouch.order.pay', 'OrderController@pay'); // 获取订单支付信息
     Route::post('ectouch.order.reminder', 'OrderController@reminder'); // 订单发货提醒
     Route::post('ectouch.order.express', 'OrderController@express'); // 订单快递追踪
     Route::post('ectouch.order.received', 'OrderController@received'); // 订单确认收货
-    Route::get('ectouch.orders.get', 'OrderController@index'); // 获取订单列表
+    Route::post('ectouch.orders.get', 'OrderController@index'); // 获取订单列表
 });
